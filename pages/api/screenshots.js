89f6bc4961d0943cef00bf01ac5d5186df1f3303
turkey <abdulrhmanturkey123@gmail.com>
2022-08-09 13:01:17 +0200

    display the screenshot as a json

--- pages/api/screenshots.js
+++ pages/api/screenshots.js
@@ -22,6 +22,6 @@ export default async function handler(req, res) {
 			res.status(200).json(response.data);
 		})
 		.catch(function (error) {
-			console.error(error);
+			res.satsus(504).json({Error: error});
 		});
 }
